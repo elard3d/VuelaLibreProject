57e65ed562fc179489f4de6f0bdbf74e2be4d00f
Elard Burga Goicochea <elard3d@hotmail.com>
2020-11-12 16:55:11 -0500

    TicketVueloMap, TicketController

---
+++ VuelaLibreProject/Models/TicketVuelo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VuelaLibreProject.Models
+{
+    public class TicketVuelo
+    {
+        public int idTicketVuelo { get; set; }
+        public int idTicket { get; set; }
+        public int idVuelo { get; set; }
+
+        public Vuelos vuelos { get; set; }
+        public Tickets tickets { get; set; }
+    }
+}
