1442df12608d8005d675bec1085e7c564159e0b5
Elard Burga Goicochea <elard3d@hotmail.com>
2020-11-12 02:12:21 -0500

    Agregar archivos de proyecto.

---
+++ VuelaLibreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VuelaLibreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
