143b0f2e68bb9d08e0c15afbfc19da2117b7026f
Elard Burga Goicochea <elard3d@hotmail.com>
2020-11-12 15:28:18 -0500

    map ticketVuelo

---
+++ VuelaLibreProject/Models/Map/TicketVueloMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VuelaLibreProject.Models.Map
+{
+    public class TicketVueloMap : IEntityTypeConfiguration<TicketVuelo>
+    {
+        public void Configure(EntityTypeBuilder<TicketVuelo> builder)
+        {
+            builder.ToTable("TicketVuelo");
+            builder.HasKey(o => o.idTicketVuelo);
+
+            builder.HasOne(o => o.vuelos).WithMany().HasForeignKey(o => o.idVuelo);
+            builder.HasOne(o => o.tickets).WithMany().HasForeignKey(o => o.idTicket);
+        }
+    }
+}
